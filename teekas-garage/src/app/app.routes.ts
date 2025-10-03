d7fd390c45fd4daa1a844a4b8cc70efbb9bb893f
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-03 16:54:48 +0200

    feat(routing): configure initial /starships route and root redirect

--- teekas-garage/src/app/app.routes.ts
+++ teekas-garage/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { StarshipsListComponent } from './features/starships/starships-list/starships-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+     path: 'starships',
+     component: StarshipsListComponent,
+    },
+
+    {
+     path: '',
+     redirectTo: 'starships',
+     pathMatch: 'full'
+    }
+
+];
---
+++ teekas-garage/src/app/features/starships/starships-list/starships-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starships-list',
+  imports: [],
+  templateUrl: './starships-list.html',
+  styleUrl: './starships-list.scss'
+})
+export class StarshipsListComponent {
+
+}
