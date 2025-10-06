670712588478aa98ee16f8328fa8b79b806e0cc4
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-06 20:25:34 +0200

    feat(starships): add router links to list items for navigation to detail page

--- teekas-garage/src/app/features/starships/starships-list/starships-list.ts
+++ teekas-garage/src/app/features/starships/starships-list/starships-list.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal, inject } from '@angular/core';
 import { StarshipsService } from '../../../core/services/starships.service';
 import { StarshipsListItem, StarshipsResponse } from '../../../core/models/starships';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-starships-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './starships-list.html',
   styleUrl: './starships-list.scss'
 })
@@ -37,5 +38,10 @@ export class StarshipsListComponent implements OnInit {
         this.loading.set(false);
       },
     });
+  }
+
+  idFromUrl(url: string): number {
+    const m = url.match(/\/starships\/(\d+)\/?$/);
+    return m ? Number(m[1]) : 0;
   }
 }
