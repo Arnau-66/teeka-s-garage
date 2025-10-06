dd421b0964debd061560a99fe1b6f2f688bab0c3
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-06 10:47:16 +0200

    feat(starships): connect service to component using signals (loading/error/data)

--- teekas-garage/src/app/features/starships/starships-list/starships-list.ts
+++ teekas-garage/src/app/features/starships/starships-list/starships-list.ts
@@ -1,12 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, inject } from '@angular/core';
+import { StarshipsService } from '../../../core/services/starships.service';
+import { StarshipsListItem, StarshipsResponse } from '../../../core/models/starships';
 
 @Component({
   selector: 'app-starships-list',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './starships-list.html',
   styleUrl: './starships-list.scss'
 })
-export class StarshipsListComponent {
 
+export class StarshipsListComponent implements OnInit {
+  loading = signal<boolean>(true);
+  error = signal<string | null>(null);
+  starships = signal<StarshipsListItem[]>([]);
+
+  private starshipsService = inject(StarshipsService);
+  ngOnInit(): void {
+    this.fetchPage(1);
+  }
+
+  fetchPage(page: number): void {
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.starshipsService.getStarships(page).subscribe({
+      next: (res: StarshipsResponse) => {
+        this.starships.set(res.results);
+      },
+      error: (err: unknown) => {
+        const msg = err instanceof Error ? err.message : 'Unexpected error';
+        this.error.set(msg);
+      },
+      complete: () => {
+        this.loading.set(false);
+      },
+    });
+  }
 }
