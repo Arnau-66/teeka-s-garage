bddcbc22af764777a4c60d7fc429d2f4efcddba1
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-06 19:44:33 +0200

    feat(routing): configure starships/:id route for StarshipDetailsComponent

--- teekas-garage/src/app/app.routes.ts
+++ teekas-garage/src/app/app.routes.ts
@@ -1,16 +1,20 @@
 import { Routes } from '@angular/router';
 import { StarshipsListComponent } from './features/starships/starships-list/starships-list';
+import { StarshipDetailsComponent } from './features/starships/starship-details/starship-details';
 
 export const routes: Routes = [
     {
-     path: 'starships',
-     component: StarshipsListComponent,
+        path: 'starships',
+        component: StarshipsListComponent,
     },
-
     {
-     path: '',
-     redirectTo: 'starships',
-     pathMatch: 'full'
+        path: 'starships/:id',
+        component: StarshipDetailsComponent,
+    },
+    {
+        path: '',
+        redirectTo: 'starships',
+        pathMatch: 'full'
     }
 
 ];
