e2d9bcafe87f02bac30e90a1a5db7f3d7981b19f
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-07 20:36:57 +0200

    chore(firebase): install and initialize Firebase SDK in main.ts

--- teekas-garage/src/environments/environment.ts
+++ teekas-garage/src/environments/environment.ts
@@ -1,6 +1,20 @@
+
+
+
+
+
 export const environment = {
     production: false,
     apiPrimary: 'https://swapi.dev/api',
     apiFallback: 'https://swapi.py4e.com/api',
     useMock: false,
+    firebase: {
+        apiKey: "AIzaSyDz-5F-YWs18kYlksmRW-CTrdTrjwfMrJM",
+        authDomain: "teeka-s-garage.firebaseapp.com",
+        projectId: "teeka-s-garage",
+        storageBucket: "teeka-s-garage.firebasestorage.app",
+        messagingSenderId: "455224848381",
+        appId: "1:455224848381:web:954653bee8381e8987b394",
+        measurementId: "G-GP4HPVQVLP"
+    },
 }
--- teekas-garage/src/main.ts
+++ teekas-garage/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { environment } from './environments/environment';
+
+const app = initializeApp(environment.firebase);
+getAuth(app);
+getFirestore(app);
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
