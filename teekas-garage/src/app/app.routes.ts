da0f4c6f0958347b2587a8cb5dff00b0c8de4a0b
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-07 16:18:26 +0200

    feat(home): add HomeComponent and make it the root route with CTA to starships list

--- teekas-garage/src/app/app.routes.ts
+++ teekas-garage/src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { StarshipsListComponent } from './features/starships/starships-list/starships-list';
 import { StarshipDetailsComponent } from './features/starships/starship-details/starship-details';
+import { HomeComponent } from './features/home/home';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'Home',
+        component: HomeComponent,
+    },
     {
         path: 'starships',
         component: StarshipsListComponent,
