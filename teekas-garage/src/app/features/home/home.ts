0f1f67fd8b0fe5770e89d391849376abc28a0edf
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-08 12:10:04 +0200

    fix(home): remove unused RouterLink and simplify HostListener for ESC handling

--- teekas-garage/src/app/features/home/home.ts
+++ teekas-garage/src/app/features/home/home.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { LoginComponent } from '../auth/login/login';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, LoginComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class HomeComponent {
 
+
+export class HomeComponent {
+  showLogin = signal(false);
+
+  openLogin() {this.showLogin.set(true);}
+  closeLogin() {this.showLogin.set(false);}
+
+  @HostListener('document:keydown.escape')
+  onEsc(){
+    if (this.showLogin()) this.closeLogin();
+  }
 }
