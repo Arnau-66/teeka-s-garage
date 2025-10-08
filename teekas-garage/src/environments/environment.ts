2cabfcf4c5f1ef5001f90f1e6dc3a864f780c6bb
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-08 16:31:03 +0200

    chore(firebase): update to rotated API key and restrict usage to localhost

--- teekas-garage/src/environments/environment.ts
+++ teekas-garage/src/environments/environment.ts
@@ -1,15 +1,11 @@
-
-
-
-
-
 export const environment = {
     production: false,
     apiPrimary: 'https://swapi.dev/api',
     apiFallback: 'https://swapi.py4e.com/api',
     useMock: false,
+
     firebase: {
-        apiKey: "AIzaSyDz-5F-YWs18kYlksmRW-CTrdTrjwfMrJM",
+        apiKey: "AIzaSyCTYVY8-0TVHL9hkt8nCYOSGS7RnQ92M7M",
         authDomain: "teeka-s-garage.firebaseapp.com",
         projectId: "teeka-s-garage",
         storageBucket: "teeka-s-garage.firebasestorage.app",
