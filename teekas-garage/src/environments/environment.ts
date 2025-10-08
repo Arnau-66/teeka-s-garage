e3d07a376d3c1ee1d672f4939b769472153bb818
Arnau Perez <arnauperezesteve@gmail.com>
2025-10-08 18:59:08 +0200

    chore(firebase): configure secure environment setup for Firebase API key

--- teekas-garage/src/environments/environment.ts
+++ teekas-garage/src/environments/environment.ts
@@ -1,16 +1,17 @@
 export const environment = {
-    production: false,
-    apiPrimary: 'https://swapi.dev/api',
-    apiFallback: 'https://swapi.py4e.com/api',
-    useMock: false,
+  production: false,
 
-    firebase: {
-        apiKey: "AIzaSyCTYVY8-0TVHL9hkt8nCYOSGS7RnQ92M7M",
-        authDomain: "teeka-s-garage.firebaseapp.com",
-        projectId: "teeka-s-garage",
-        storageBucket: "teeka-s-garage.firebasestorage.app",
-        messagingSenderId: "455224848381",
-        appId: "1:455224848381:web:954653bee8381e8987b394",
-        measurementId: "G-GP4HPVQVLP"
-    },
-}
+  apiPrimary: 'https://swapi.dev/api',
+  apiFallback: 'https://swapi.py4e.com/api',
+  useMock: false,
+
+  firebase: {
+    apiKey: 'AIzaSyCTYVY8-0TVHL9hkt8nCYOSGS7RnQ92M7M',
+    authDomain: 'teeka-s-garage.firebaseapp.com',
+    projectId: 'teeka-s-garage',
+    storageBucket: 'teeka-s-garage.firebasestorage.app',
+    messagingSenderId: '455224848381',
+    appId: '1:455224848381:web:954653bee8381e8987b394',
+    measurementId: 'G-GP4HPVQVLP'
+  }
+};
